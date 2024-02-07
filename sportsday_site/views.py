9ac88802cdcea73e3e462e23209596cfc3bf520f
Isaac Hew <isaachewks@gmail.com>
2024-02-07 20:45:40 +0800

    Implemented the results entry system
    Assigns the input to the result field if it is a valid number and the 
    fail type field if it is not

--- sportsday_site/views.py
+++ sportsday_site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Student,House,Event,Category
+from django.http import HttpResponse
+from .models import Student,House,Signup,Event,Category
 
 
 # Create your views here
@@ -10,6 +11,41 @@ def list_events(request,**kwargs):
 
 def event_details(request,event_pk):
     event=Event.objects.get(pk=event_pk)
+    if request.method=="POST":
+        signups=Signup.objects.filter(signed_event=event_pk)
+        for signup in signups:
+            # is each result set for this signup?
+            if "result1_"+str(signup.pk) in request.POST:
+                result=request.POST["result1_"+str(signup.pk)]
+                if result.replace(".","",1).isnumeric():# is it a valid number?
+                    signup.result1=float(result)
+                    signup.result1_fail_type=""
+                else:
+                    signup.result1=None
+                    signup.result1_fail_type=result
+            if "result2_"+str(signup.pk) in request.POST:
+                result=request.POST["result2_"+str(signup.pk)]
+                if result.replace(".","",1).isnumeric():# is it a valid number?
+                    signup.result2=float(result)
+                    signup.result2_fail_type=""
+                else:
+                    signup.result2=None
+                    signup.result2_fail_type=result
+            if "result3_"+str(signup.pk) in request.POST:
+                result=request.POST["result3_"+str(signup.pk)]
+                if result.replace(".","",1).isnumeric():# is it a valid number?
+                    signup.result3=float(result)
+                    signup.result3_fail_type=""
+                else:
+                    signup.result3=None
+                    signup.result3_fail_type=result
+            # Save each signup
+            signup.save(update_fields=[
+                "result1","result1_fail_type",
+                "result2","result2_fail_type",
+                "result3","result3_fail_type"
+            ])
+
     return render(request, "event_details.html", {"event":event})
 
 def student_details(request, student_pk=1):
